47254bad5632e5643361108f5bba0d6de0d479ee
Keylin_D <keylin.05.01@gmail.com>
2018-07-10 09:32:54 -0400

    creando la primera funcion

---
+++ app.js
@@ -0,0 +1,12 @@
+function logInOrRegister() {
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+  .then(() => {
+    console.log("Usuario registrado");
+  })
+  .catch((error) => {
+    console.log("Error de firebase >" +error.code);
+    console.log("Error de firebase, mensaje >" +error.message);
+  });
+}
