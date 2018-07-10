329b82d8b6aa363def77edcd3f2b8deaa88e888f
Keylin_D <keylin.05.01@gmail.com>
2018-07-10 12:30:38 -0400

    mostrando mensajes en la pantalla

--- app.js
+++ app.js
@@ -1,61 +1,91 @@
-window.onload = () =>{
-  
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      //Si estamos logueados
-      loggedOut.style.display = "none";
-      loggedIn.style.display = "block";
-      console.log("User >" +JSON.stringify(user));
-    }else{
-      //No estamos logueados
-      loggedOut.style.display = "block";
-      loggedIn.style.display = "none";
-    }
+window.onload = ()=>{
+  firebase.auth().onAuthStateChanged((user)=>{
+      if(user){
+          //Si estamos logueados
+          loggedOut.style.display = "none";
+          loggedIn.style.display = "block";
+          console.log("User > "+JSON.stringify(user));
+      }else{
+          //No estamos logueados
+          loggedOut.style.display = "block";
+          loggedIn.style.display = "none";
+      }
   });
+
+  //Acá comenzamos a escuchar por nuevos mensajes usando el evento
+  //on child_added
+  firebase.database().ref('messages')
+      .on('child_added', (newMessage)=>{
+          messageContainer.innerHTML += `
+              <p>Nombre : ${newMessage.val().creatorName}</p>
+              <p>${newMessage.val().text}</p>
+          `;
+      });
 };
-function register() {
+
+function register(){
+  const emailValue = email.value;
+  const passwordValue = password.value; 
+  firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
+      .then(()=>{
+          console.log("Usuario registrado");
+      })
+      .catch((error)=>{
+          console.log("Error de firebase > "+error.code);
+          console.log("Error de firebase, mensaje > "+error.message);
+      });
+}
+
+function login(){
   const emailValue = email.value;
   const passwordValue = password.value;
-  firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
-  .then(() => {
-    console.log("Usuario registrado");
-  })
-  .catch((error) => {
-    console.log("Error de firebase >" +error.code);
-    console.log("Error de firebase, mensaje >" +error.message);
-  });
+  firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+      .then(()=>{
+          console.log("Usuario con login exitoso");
+      })
+      .catch((error)=>{
+          console.log("Error de firebase > "+error.code);
+          console.log("Error de firebase, mensaje > "+error.message);
+      });
 }
-  function login() {
-    const emailValue = email.value;
-    const passwordValue = password.value;
-    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
-    .then(() => {
-      console.log("Usuario con login exitoso");
-    })
-    .catch((error) => {
-      console.log("Error de firebase >" +error.code);
-      console.log("Error de firebase, mensaje >" +error.message);
-    });
-  }
-function logout() {
+
+function logout(){
   firebase.auth().signOut()
-  .then(() => {
-    console.log("chaoo");
-  })
-  .catch();
+      .then(()=>{
+          console.log("Chao");
+      })
+      .catch();
 }
+
 function loginFacebook(){
   const provider = new firebase.auth.FacebookAuthProvider();
-  // provider.addScope("user_birthday"); tienen que perdirle permiso a facebook
+  //provider.addScope("user_birthday"); tienen que pedirle permiso a facebook
   provider.setCustomParameters({
-    'display': 'popup'
-  });
+      'display': 'popup'
+  }); 
   firebase.auth().signInWithPopup(provider)
-  .then(() => {
-    console.log("Login con facebook");
-  })
-  .catch((error) => {
-    console.log("Error de firebase >" +error.code);
-    console.log("Error de firebase, mensaje >" +error.message);
+      .then(()=>{
+          console.log("Login con facebook");
+      })
+      .catch((error)=>{
+          console.log("Error de firebase > "+error.code);
+          console.log("Error de firebase, mensaje > "+error.message);
+      });
+}
+
+// Firebase Database
+
+// Usaremos una colección para guardar los mensajes, llamada messages
+function sendMessage(){
+  const currentUser = firebase.auth().currentUser;
+  const messageAreaText = messageArea.value;
+
+  //Para tener una nueva llave en la colección messages
+  const newMessageKey = firebase.database().ref().child('messages').push().key;
+
+  firebase.database().ref(`messages/${newMessageKey}`).set({
+      creator : currentUser.uid,
+      creatorName : currentUser.displayName,
+      text : messageAreaText
   });
 }
